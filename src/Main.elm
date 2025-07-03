a72182781cfabddd2d25ed0e64c50fd284146de0
Akash Kurdekar <akash.kurdekar@sedex.com>
2025-07-03 14:42:04 +0100

    added some styles

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (Msg(..), main, update, view)
 
 import Browser
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 
 
@@ -41,24 +42,62 @@ update msg model =
                 initialModel
 
             else
-                { counter = model.counter + 1 }
+                { counter = model.counter - 1 }
 
         Reset ->
             initialModel
 
 
+containerStyles : List (Html.Attribute msg)
+containerStyles =
+    [ style "display" "flex"
+    , style "flex-direction" "column"
+    , style "align-items" "center"
+    , style "justify-content" "center"
+    , style "height" "100vh"
+    , style "font-family" "sans-serif"
+    ]
+
+
+rowStyles : List (Html.Attribute msg)
+rowStyles =
+    [ style "display" "flex"
+    , style "align-items" "center"
+    , style "gap" "16px"
+    ]
+
+
+buttonStyles : List (Html.Attribute msg)
+buttonStyles =
+    [ style "font-size" "2rem"
+    , style "width" "48px"
+    , style "height" "48px"
+    ]
+
+
+counterStyles : List (Html.Attribute msg)
+counterStyles =
+    [ style "font-size" "2rem"
+    , style "width" "48px"
+    , style "text-align" "center"
+    ]
+
+
+resetButtonStyles : List (Html.Attribute msg)
+resetButtonStyles =
+    [ style "margin-top" "24px"
+    , style "padding" "8px 24px"
+    , style "font-size" "1rem"
+    ]
+
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ div []
-            [ button [ onClick Decrement ] [ text "-" ]
-            , div []
-                [ text (String.fromInt model.counter) ]
-            , button
-                [ onClick Increment ]
-                [ text "+" ]
+    div containerStyles
+        [ div rowStyles
+            [ button (onClick Decrement :: buttonStyles) [ text "-" ]
+            , div counterStyles [ text (String.fromInt model.counter) ]
+            , button (onClick Increment :: buttonStyles) [ text "+" ]
             ]
-        , button
-            [ onClick Reset ]
-            [ text "RESET" ]
+        , button (onClick Reset :: resetButtonStyles) [ text "RESET" ]
         ]
