9ef23aaa297a2aa54345968816c476172ebbb986
Akash Kurdekar <akash.kurdekar@sedex.com>
2025-07-03 12:20:54 +0100

    using a record

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,19 @@
 module Main exposing (Msg(..), main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (checked, style, type_)
+import Html.Events exposing (onCheck, onClick)
 
 
-initialModel : Int
+type alias Model =
+    { counterValue : Int
+    }
+
+
+initialModel : Model
 initialModel =
-    0
+    { counterValue = 0 }
 
 
 main =
@@ -24,36 +30,81 @@ type Msg
     | Reset
 
 
-update : Msg -> Int -> Int
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            { model | counterValue = model.counterValue + 1 }
 
         Decrement ->
-            if model == initialModel then
+            if model.counterValue == initialModel.counterValue then
                 initialModel
 
             else
-                model - 1
+                { model | counterValue = model.counterValue - 1 }
 
         Reset ->
             initialModel
 
 
+containerStyle : List (Html.Attribute msg)
+containerStyle =
+    [ style "display" "flex"
+    , style "flex-direction" "column"
+    , style "align-items" "center"
+    , style "justify-content" "center"
+    , style "height" "100vh"
+    ]
+
+
+counterRowStyle : List (Html.Attribute msg)
+counterRowStyle =
+    [ style "display" "flex"
+    , style "align-items" "center"
+    , style "gap" "16px"
+    ]
+
+
+buttonStyle : List (Html.Attribute msg)
+buttonStyle =
+    [ style "font-size" "2rem"
+    , style "padding" "0.5rem 1rem"
+    ]
+
+
+countStyle : List (Html.Attribute msg)
+countStyle =
+    [ style "font-size" "2rem"
+    , style "width" "2.5rem"
+    , style "text-align" "center"
+    ]
+
+
+resetButtonStyle : List (Html.Attribute msg)
+resetButtonStyle =
+    [ style "margin-top" "24px"
+    , style "padding" "0.5rem 1.5rem"
+    , style "font-size" "1rem"
+    ]
+
+
+view : Model -> Html Msg
 view model =
-    div []
-        [ div []
-            [ button
-                [ onClick Decrement ]
-                [ text "-" ]
-            , div []
-                [ text (String.fromInt model) ]
-            , button
-                [ onClick Increment ]
-                [ text "+" ]
+    div containerStyle
+        [ div counterRowStyle
+            [ button (onClick Decrement :: buttonStyle) [ text "-" ]
+            , div countStyle [ text (String.fromInt model.counterValue) ]
+            , button (onClick Increment :: buttonStyle) [ text "+" ]
             ]
-        , button
-            [ onClick Reset ]
-            [ text "RESET" ]
+
+        -- , div []
+        --     [ input
+        --         [ type_ "checkbox"
+        --         , checked model.checked
+        --         , onCheck ToggleCheck
+        --         ]
+        --         []
+        --     , text " Check me!"
+        --     ]
+        , button (onClick Reset :: resetButtonStyle) [ text "RESET" ]
         ]
